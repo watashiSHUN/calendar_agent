014f7d70e4ba6e23eb7c8dad15cfa719b9de1e45
Shun Gram <mike53669@gmail.com>
2025-03-17 20:09:35 -0700

    quick starter python script calling google calendar

--- add_event_to_calendar.py
+++ add_event_to_calendar.py
@@ -5,16 +5,24 @@ from openai import OpenAI
 
 load_dotenv()
 
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
-completion = client.chat.completions.create(
-    model="gpt-4",
-    messages=[
-        {
-            "role": "user",
-            "content": "Write a one-sentence bedtime story about a unicorn.",
-        }
-    ],
-)
+# NOTE(shunxian): we need a GCP project
+# application(serviceaccount) to act on people's calendar
+def CallGoogleCalendar():
+    pass
 
-print(completion.choices[0].message.content)
+
+def CallOpenAI():
+    client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+    completion = client.chat.completions.create(
+        model="gpt-4",
+        messages=[
+            {
+                "role": "user",
+                "content": "Write a one-sentence bedtime story about a unicorn.",
+            }
+        ],
+    )
+
+    print(completion.choices[0].message.content)
